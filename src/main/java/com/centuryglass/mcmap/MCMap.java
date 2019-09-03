fa96d88ad59e8177c6d2fb8010fed6387d10d6b5
centuryglass <anthony0857@gmail.com>
2019-09-02 23:01:12 -0500

    Improved command line interface, added controls for extra features.
    MCMap now provides more options for generating tiled maps. Main now uses
    more robust argument handling methods, and provides additional options
    for configuring map output.
    
    MapUnit and Pair utility classes added for convenient unit conversion
    and generic data bundling.

--- src/main/java/com/centuryglass/mcmap/MCMap.java
+++ src/main/java/com/centuryglass/mcmap/MCMap.java
@@ -6,14 +6,17 @@
 package com.centuryglass.mcmap;
 
 import com.centuryglass.mcmap.mapping.MapCollector;
+import com.centuryglass.mcmap.mapping.TileMap;
 import com.centuryglass.mcmap.savedata.MCAFile;
 import com.centuryglass.mcmap.threads.MapperThread;
 import com.centuryglass.mcmap.threads.ProgressThread;
 import com.centuryglass.mcmap.threads.ReaderThread;
 import java.awt.Point;
 import java.io.File;
-import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  *  MCMap defines the main set of actions necessary to process all region files
@@ -22,113 +25,250 @@ import java.util.ArrayList;
  * server plugin.
  */
 public class MCMap
-{
-    // Default values:
-    // TODO: load these from a config file
-    private static final int WORLD_BORDER = 1600;
-    private static final int MIN_SIZE = 256;
-    private static final int MAX_SIZE = 10000;
-
+{   
+    // Debug: Set whether to use multiple threads to scan region files:
+    private static final boolean MULTI_REGION_THREADS = true;
+    
+    /**
+     * Gets the bounds of all regions within a Minecraft save directory.
+     * 
+     * @param regionDataDir The path to a Minecraft region file directory.
+     * 
+     * @return  The upper left and lower right chunk coordinates of the
+     *          rectangle that contains all regions.
+     */
+    public static Pair<Point, Point> getWorldBounds(File regionDataDir)
+    {
+        File[] directoryFiles = regionDataDir.listFiles();
+        int xMin = Integer.MAX_VALUE;
+        int zMin = Integer.MAX_VALUE;
+        int xMax = Integer.MIN_VALUE;
+        int zMax = Integer.MIN_VALUE;
+        for (File regionFile : directoryFiles)
+        {
+            Point chunkCoords = MCAFile.getChunkCoords(regionFile);
+            xMin = Math.min(xMin, chunkCoords.x);
+            zMin = Math.min(zMin, chunkCoords.y);
+            xMax = Math.max(xMax, chunkCoords.x);
+            zMax = Math.max(zMax, chunkCoords.y);
+        }
+        if (xMin == Integer.MAX_VALUE || zMin == Integer.MAX_VALUE
+                || xMax == Integer.MIN_VALUE || zMax == Integer.MIN_VALUE)
+        {
+            return new Pair(new Point(0, 0), new Point(0, 0));
+        }
+        return new Pair(
+                MapUnit.convertPoint(new Point(xMin, zMin),
+                        MapUnit.REGION, MapUnit.CHUNK),
+                MapUnit.convertPoint(new Point(xMax, zMax),
+                        MapUnit.REGION, MapUnit.CHUNK));
+    }
+    
+    /**
+     * Maps all world data within a set of tiled image maps.
+     * 
+     * @param tileSize         The width and height of each map tile in chunks.
+     *                         Multiples of 32 will provide the best
+     *                         performance, as that ensures each region file's
+     *                         data will fit within a single tile.
+     * 
+     * @param pixelsPerChunk   Width and height in pixels of each chunk within
+     *                         generated map images.
+     * 
+     * @param regionDataDir    The Minecraft region file directory used to
+     *                         generate maps.
+     * 
+     * @param imageDir         The directory where map images will be saved.
+     * 
+     * @param baseName         The start of the filename that will be used to
+     *                         save all maps. Each map tile will be saved at
+     *                         baseName.X.Z.png within a subdirectory specific
+     *                         to its map type.
+     * 
+     * @param dirInfoFile      A file containing notable coordinates to mark on
+     *                         the server directory map. Directory files should
+     *                         list one point per line, formatted as 
+     *                         X Z PlaceName.
+     */
+    public static void createTileMaps(int tileSize,
+            int pixelsPerChunk,
+            File regionDataDir,
+            File imageDir,
+            String baseName,
+            File dirInfoFile)
+    {
+        ArrayList<File> regionFiles = new ArrayList(Arrays.asList(
+                regionDataDir.listFiles()));
+        MapCollector mappers = new MapCollector(imageDir, baseName, tileSize,
+                pixelsPerChunk, dirInfoFile);
+        // If more than one tile fits in a region, sort regions by tile:
+        if (tileSize > 32)
+        {
+            class RegionSort implements Comparator<File>
+            {
+                // Get the coordinates of the tile that hold a region file's
+                // upper left point.
+                private Point getTilePt(File regionFile)
+                {
+                    Point chunkPt = MCAFile.getChunkCoords(regionFile);
+                    return TileMap.getTilePoint(chunkPt.x, chunkPt.y, tileSize);
+                }
+                
+                @Override
+                public int compare(File first, File second)
+                {
+                    Point firstTile = getTilePt(first);
+                    Point secondTile = getTilePt(second);
+                    if (firstTile.y == secondTile.y)
+                    {
+                        return firstTile.x - secondTile.x;
+                    }
+                    return firstTile.y - secondTile.y;
+                }
+            }
+            Collections.sort(regionFiles, new RegionSort());
+        }
+        final int chunksMapped = mapRegions(regionFiles, mappers);
+        if (chunksMapped > 0)
+        {
+            int mapKM = MapUnit.convert(chunksMapped, MapUnit.CHUNK,
+                    MapUnit.BLOCK) / 1000000;
+            System.out.println("Mapped " + chunksMapped
+                    + " chunks, an area of " + mapKM + " km^2.");        
+        }
+    }
 
     /**
      * Generates all map variants from Minecraft server data.
      * 
-     * @param mapEdge          Width and height of the mapped region, centered
-     *                         on (0, 0) and measured in chunks. This value may
-     *                         be adjusted if the resulting maps would be
-     *                         excessively large.
+     * @param xMin             Lowest x-coordinate within the mapped area,
+     *                         measured in chunks.
+     * 
+     * @param zMin             Lowest z-coordinate within the mapped area,
+     *                         measured in chunks.
+     * 
+     * @param width            Width of the mapped area, measured in chunks.
+     * 
+     * @param height           Height of the mapped area, measured in chunks.
      * 
      * @param pxPerChunk       Width and height in pixels of each chunk within
-     *                         generated map images. This value may be adjusted
-     *                         if the resulting maps would be excessively large.
+     *                         generated map images.
      * 
-     * @param regionDataPath   Path to the Minecraft region file directory used
-     *                         to generate maps.
+     * @param regionDataDir    The Minecraft region file directory used to
+     *                         generate maps.
      * 
-     * @param imagePath        Path where image files will be saved. Use a
-     *                         generic path value like "./map.png", individual
-     *                         map types will be saved to "./map_type.png".
+     * @param imageDir         The directory where map images will be saved.
      * 
-     * @param dirInfoPath      Path to a file containing notable coordinates to
-     *                         mark on the server directory map. Directory files
-     *                         should list one point per line, formatted as
+     * @param imagePrefix      The start of the filename that will be used to
+     *                         save all maps. Each map type will be saved at
+     *                         imagePrefix + "MapType" + ".png"
+     * 
+     * @param dirInfoFile      A file containing notable coordinates to mark on
+     *                         the server directory map. Directory files should
+     *                         list one point per line, formatted as 
      *                         X Z PlaceName.
      */
-    public static void createMaps(int mapEdge, int pxPerChunk,
-            Path regionDataPath, Path imagePath, Path dirInfoPath)
+    public static void createMaps(
+            int xMin,
+            int zMin,
+            int width,
+            int height,
+            int pxPerChunk,
+            File regionDataDir,
+            File imageDir,
+            String imagePrefix,
+            File dirInfoFile)
     {
-        // save region file paths and count:
-        ArrayList<Path> regionFiles = new ArrayList();
-        int maxDistanceFromOrigin = 0;
-        int maxAllowed = WORLD_BORDER;
+        ArrayList<File> regionFiles = new ArrayList();
+        int xMax = xMin + width;
+        int zMax = zMin + height;
+        int regionChunks = MapUnit.convert(1, MapUnit.REGION, MapUnit.CHUNK);
         int outOfBounds = 0;
-        File regionDir = regionDataPath.toFile();
-        File[] directoryFiles = regionDir.listFiles();
-        for (File regionFile : directoryFiles)
+        // Valid region files within the given bounds will all be named
+        // r.X.Z.mca, where X and Z are valid region coordinates within the
+        // given range. Check each of these names to see if a corresponding file
+        // exists.
+        int regionXMin = (int) Math.floor((double) xMin 
+                / (double) regionChunks);
+        int regionXMax = (int) Math.ceil((double) xMin / (double) regionChunks)
+                + (int) Math.ceil((double) width / (double) regionChunks);
+        int regionZMin = (int) Math.floor((double) zMin
+                / (double) regionChunks);
+        int regionZMax = (int) Math.ceil((double) zMin / (double) regionChunks)
+                + (int) Math.ceil((double) height / (double) regionChunks);
+        for(int x = regionXMin; x < regionXMax; x++)
         {
-            // find maximum distance from origin:
-            int fileMax = 0;
-            Point chunkCoords = MCAFile.getChunkCoords(regionFile.toPath());
-            if (chunkCoords.x == -1 && chunkCoords.y == -1)
+            for (int z = regionZMin; z < regionZMax; z++)
             {
-                System.err.println(regionFile.getPath() 
-                        + " does not have a legal region file name format.");
-                continue;
+                String filename = "r." + String.valueOf(x) + "."
+                        + String.valueOf(z) + ".mca";
+                File regionFile = new File(regionDataDir, filename);
+                if (regionFile.exists())
+                {
+                    regionFiles.add(regionFile);
+                }
             }
-            fileMax = Math.max(fileMax, (chunkCoords.x >= 0 ?
-                        (chunkCoords.x + 32) : (chunkCoords.x * -1)));
-            fileMax = Math.max(fileMax, (chunkCoords.y >= 0 ?
-                        (chunkCoords.y + 32) : (chunkCoords.y * -1)));
-            if (fileMax <= maxAllowed)
-            {
-                maxDistanceFromOrigin = Math.max(fileMax,
-                        maxDistanceFromOrigin);
-                regionFiles.add(regionFile.toPath());
-            }
-            else
-            {
-                outOfBounds++;
-            }        
         }
-        if (outOfBounds > 0)
+        int numRegionFiles = regionFiles.size();    
+        if (numRegionFiles == 0)
         {
-            System.err.println("Warning: " + outOfBounds
-                    + " region files past the world border at "
-                    + (WORLD_BORDER * 16) + " will be ignored.");
+            System.out.println("Region was empty, no maps were created.");
+            return;          
         }
-        mapEdge = maxDistanceFromOrigin * 2;
-        // Ensure map sizes fit within the maximum image size:
-        while((mapEdge * pxPerChunk) < MIN_SIZE)
+        final MapCollector mappers = new MapCollector(imageDir, imagePrefix,
+                xMin, zMin, width, height, pxPerChunk, dirInfoFile);
+        final int chunksMapped = mapRegions(regionFiles, mappers);
+        if (chunksMapped > 0)
         {
-            pxPerChunk++;
+            int numChunks = width * height;
+            double explorePercent = (double) chunksMapped * 100 / numChunks;
+            System.out.println("Mapped " + chunksMapped
+                    + " chunks out of " + numChunks + ", map is "
+                    + explorePercent
+                    + "% explored.");
         }
-        while((mapEdge * pxPerChunk) > MAX_SIZE && pxPerChunk > 1)
+        else
         {
-            pxPerChunk--;
+            System.out.println("Region was empty, no maps were created.");
         }
-        if((mapEdge * pxPerChunk) > MAX_SIZE)
-        {
-            mapEdge = MAX_SIZE;
-            int maxBlock = (mapEdge / 2) * 32 * 16;
-            System.out.println(
-                    "Warning: Map would exceed the maximum image size of "
-                    + MAX_SIZE + " x " + MAX_SIZE + ", chunks further than "
-                    + maxBlock + " blocks from (0,0) will be cropped.");
-        }
+    }
+    
+    /**
+     * Finishes the process of mapping a set of Minecraft regions, processing
+     * all regions within multiple threads.
+     * 
+     * @param regionFiles  The set of Minecraft region files to map.
+     * 
+     * @param mapper       The object responsible for copying world data into
+     *                     map images.
+     * 
+     * @return             The total number of region chunks mapped.
+     */
+    private static int mapRegions
+    (ArrayList<File> regionFiles, MapCollector mapper)
+    {   
         int numRegionFiles = regionFiles.size();
-        // Initialize Mappers with the provided path:
-        final MapCollector mappers = new MapCollector(imagePath, dirInfoPath,
-                mapEdge, mapEdge, pxPerChunk);
-        
         // Provide threadsafe tracking of processed region and chunk counts:
         ProgressThread progressThread = new ProgressThread(numRegionFiles);
         progressThread.start();
         // Handle all map updates within a single thread:
-        MapperThread mapperThread = new MapperThread(mappers);
+        MapperThread mapperThread = new MapperThread(mapper);
         mapperThread.start();
         // Divide region file updates between multiple threads:
-        int numReaderThreads = Math.max(1,
-                Runtime.getRuntime().availableProcessors());
+        int numReaderThreads;
+        if (MULTI_REGION_THREADS)
+        {
+            numReaderThreads = Math.max(1,
+                    Runtime.getRuntime().availableProcessors());
+        }
+        else
+        {
+            numReaderThreads = 1;
+        }
+        if (numReaderThreads > numRegionFiles)
+        {
+            numReaderThreads = numRegionFiles;
+        }
         System.out.println("Processing " + numRegionFiles
                 + " region files with " + numReaderThreads + " threads.");
         int filesPerThread = numRegionFiles / numReaderThreads;
@@ -158,24 +298,24 @@ public class MCMap
             }
         }
         mapperThread.requestStop();
-        progressThread.requestStop();
         while (mapperThread.isAlive())
         {
             try
             {
                 mapperThread.join();
             }
-            catch (InterruptedException e)
-            {
-                // Keep trying...
-            }
+            catch (InterruptedException e) { }
         }
-        int numChunks = mapEdge * mapEdge;
-        double explorePercent = (double) progressThread.getChunkCount() * 100
-                / numChunks;
-        System.out.println("Mapped " + progressThread.getChunkCount()
-                + " chunks out of " + numChunks + ", map is " + explorePercent
-                + "% explored.");
-        mappers.saveMapFile();
+        progressThread.requestStop();
+        while (progressThread.isAlive())
+        {
+            try
+            {
+                progressThread.join();
+            }
+            catch (InterruptedException e) { }
+        }
+        mapper.saveMapFile();
+        return progressThread.getChunkCount();
     }
 }
---
+++ src/main/java/com/centuryglass/mcmap/Pair.java
@@ -0,0 +1,42 @@
+/**
+ * @file Pair.java
+ * 
+ * Holds two objects of any two types.
+ */
+package com.centuryglass.mcmap;
+
+/**
+ * Allows any two object types to be packaged together, similar to the std::pair
+ * class in C++.
+ * 
+ * @param <FirstType>   The first item's type.
+ * 
+ * @param <SecondType>  The second item's type.
+ */
+public class Pair <FirstType, SecondType> 
+{
+    public FirstType first;
+    public SecondType second;
+    
+    /**
+     * Initializes both pair values on construction.
+     * 
+     * @param first   The first object.
+     * 
+     * @param second  The second object. 
+     */
+    public Pair(FirstType first, SecondType second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+    
+    /**
+     * Initializes the Pair with null values.
+     */
+    public Pair()
+    {
+        first = null;
+        second = null;
+    }
+}
