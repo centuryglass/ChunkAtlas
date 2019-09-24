356f993ada7aa8722717b318e7d4b3f81fda9182
centuryglass <anthony0857@gmail.com>
2019-09-24 00:09:35 -0500

    Updated ActivityMapper to use ColorRangeFactory and ColorRangeSet to select values.

--- src/main/java/com/centuryglass/mcmap/mapping/ActivityMapper.java
+++ src/main/java/com/centuryglass/mcmap/mapping/ActivityMapper.java
@@ -6,13 +6,18 @@
 
 package com.centuryglass.mcmap.mapping;
 
+import com.centuryglass.mcmap.TickDuration;
+import com.centuryglass.mcmap.images.ColorRangeFactory;
+import com.centuryglass.mcmap.images.ColorRangeSet;
 import com.centuryglass.mcmap.worldinfo.ChunkData;
 import java.awt.Color;
 import java.awt.Point;
 import java.io.File;
-import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * ActivityMapper a map showing the relative amount of time that players have
@@ -20,34 +25,7 @@ import java.util.Map;
  */
 public class ActivityMapper extends Mapper
 {
-    public static final Color MIN_ACTIVITY = Color.BLACK;
-    public static final Color HIGH_ACTIVITY = Color.YELLOW;
-    public static final Color MAX_ACTIVITY = Color.WHITE;
-    public static final Color [] ACTIVITY_LEVEL_COLORS;
-    public static long [] ACTIVITY_LEVEL_TIMES;
-    static
-    {
-        ACTIVITY_LEVEL_COLORS = new Color[5];
-        ACTIVITY_LEVEL_TIMES = new long[5];
-        
-        // Time periods, expressed in tick counts:
-        final long second = 20;
-        final long minute = second * 60;
-        final long hour = minute * 60;
-        final long day = hour * 24;
-        
-        ACTIVITY_LEVEL_COLORS[0] = Color.BLUE;
-        ACTIVITY_LEVEL_COLORS[1] = Color.CYAN;
-        ACTIVITY_LEVEL_COLORS[2] = Color.MAGENTA;
-        ACTIVITY_LEVEL_COLORS[3] = Color.RED;
-        ACTIVITY_LEVEL_COLORS[4] = Color.ORANGE;
-        
-        ACTIVITY_LEVEL_TIMES[0] = minute;
-        ACTIVITY_LEVEL_TIMES[1] = minute * 10;
-        ACTIVITY_LEVEL_TIMES[2] = hour;
-        ACTIVITY_LEVEL_TIMES[3] = hour * 5;
-        ACTIVITY_LEVEL_TIMES[4] = day;
-    }
+    private static final double MIN_COLOR_INTENSITY = 0.25;
     
     /**
      * Initializes a mapper that creates a single activity map.
@@ -95,9 +73,10 @@ public class ActivityMapper extends Mapper
     /**
      * Saves the inhabited time of a chunk so it can be drawn later.
      *
-     * @param chunk  The chunk that may be drawn.
+     * @param chunk  Data for a new map chunk.
      *
-     * @return       The color 
+     * @return       Null, as chunk colors are only calculated once the full
+     *               range of inhabited times has been found.
      */
     @Override
     public Color getChunkColor(ChunkData chunk)
@@ -106,31 +85,11 @@ public class ActivityMapper extends Mapper
         {
             return null;
         }
-        Point chunkPt = chunk.getPos();
         long inhabitedTime = chunk.getInhabitedTime();
+        Point chunkPt = chunk.getPos();
+        inhabitedTimes.put(chunkPt, inhabitedTime);
         maxTime = Math.max(inhabitedTime, maxTime);
-        if (inhabitedTime > ACTIVITY_LEVEL_TIMES[
-                ACTIVITY_LEVEL_TIMES.length - 1])
-        {
-            inhabitedTimes.put(chunkPt, inhabitedTime);
-        }
-        if (inhabitedTime == 0)
-        {
-            return MIN_ACTIVITY;
-        }
-        for (int i = 0; i < ACTIVITY_LEVEL_TIMES.length; i++)
-        {
-            if (inhabitedTime <= ACTIVITY_LEVEL_TIMES[i])
-            {
-                final Color levelColor = ACTIVITY_LEVEL_COLORS[i];
-                final double strengthMult = (double) inhabitedTime 
-                        / (double) ACTIVITY_LEVEL_TIMES[i];
-                return new Color((int) (levelColor.getRed() * strengthMult),
-                        (int) (levelColor.getGreen() * strengthMult),
-                        (int) (levelColor.getBlue() * strengthMult), 255);
-            }
-        }
-        return HIGH_ACTIVITY;
+        return null;
     }
 
     /**
@@ -141,29 +100,71 @@ public class ActivityMapper extends Mapper
     @Override
     protected void finalProcessing(WorldMap map)
     {
-        int totalChunks = 0;
+        ArrayList<Color> rangeColors = new ArrayList();
+        BiFunction<Color, Double, Color> setMagnitude = (color, fraction) ->
+        {
+            if (fraction < 0) { fraction = 0.0; }
+            else if (fraction > 1) { fraction = 1.0; }
+            return new Color((int) (color.getRed() * fraction),
+                    (int) (color.getGreen() * fraction),
+                    (int) (color.getBlue() * fraction));
+                    
+        };
+        rangeColors.add(Color.YELLOW);
+        rangeColors.add(Color.RED);
+        rangeColors.add(Color.MAGENTA);
+        rangeColors.add(Color.GREEN);
+        rangeColors.add(Color.CYAN);
+        rangeColors.add(Color.BLUE);
+        ColorRangeFactory rangeFactory = new ColorRangeFactory(
+                inhabitedTimes.values(), rangeColors);
+        rangeFactory.setFadeFraction(MIN_COLOR_INTENSITY);
+        rangeFactory.setFadeType(ColorRangeSet.FadeType.TO_BLACK);
+        Function <Long, Long> roundToNextUnit = tickCount -> 
+        {
+            final long[] unitDurations =
+            {
+                TickDuration.fromYears(1).asTicks,
+                TickDuration.fromWeeks(1).asTicks,
+                TickDuration.fromDays(1).asTicks,
+                TickDuration.fromHours(1).asTicks,
+                TickDuration.fromMinutes(1).asTicks,
+                TickDuration.fromSeconds(1).asTicks,
+                1
+            };
+            for (long duration : unitDurations)
+            {
+                if (tickCount == duration) 
+                {
+                    return duration;
+                }
+                else if (tickCount > duration)
+                {
+                    return duration * (tickCount / duration + 1);
+                }
+            }
+            return 0L;
+        };
+        rangeFactory.setRangeAdjuster(roundToNextUnit);
+        ColorRangeSet colorRanges = rangeFactory.createColorRangeSet();
+        ColorRangeSet.Range[] rangeDescriptionList = colorRanges.getRanges();
+        for (ColorRangeSet.Range range : rangeDescriptionList)
+        {
+            TickDuration rangeMax = new TickDuration(range.maxValue);
+            System.out.println(rangeMax.toString() + " or less: "
+                    + range.maxColor.toString());
+        }
         for (Map.Entry<Point, Long> entry : inhabitedTimes.entrySet())
         {
             final int x = entry.getKey().x;
             final int z = entry.getKey().y;
             final long mapValue = entry.getValue();
-            final double strengthMult = (double) mapValue / (double) maxTime;
-            final int r = (int) (MAX_ACTIVITY.getRed() * strengthMult
-                    + HIGH_ACTIVITY.getRed() * (1 - strengthMult));
-            final int g = (int) (MAX_ACTIVITY.getGreen() * strengthMult
-                    + HIGH_ACTIVITY.getGreen() * (1 - strengthMult));
-            final int b = (int) (MAX_ACTIVITY.getBlue() * strengthMult
-                    + HIGH_ACTIVITY.getBlue() * (1 - strengthMult));
-            map.setChunkColor(x, z, new Color(r, g, b, 255));
+            map.setChunkColor(x, z, colorRanges.getValueColor(mapValue));
         }
         super.finalProcessing(map);
-        Duration maxDuration = Duration.ofSeconds(maxTime * 20);
+        TickDuration maxDuration = new TickDuration(maxTime);
         System.out.println("The highest inhabited time of all chunks is "
-                + (maxDuration.toDays() / 365) + " years,"
-                + (maxDuration.toDays() % 365) + " days, "
-                + (maxDuration.toHours() % 24) + " hours, "
-                + (maxDuration.toMinutes() % 60) + " minutes, "
-                + (maxDuration.getSeconds() % 60) + " seconds.");
+                + maxDuration.toString() + ".");
     }
 
     // Inhabited times for all map chunks:
